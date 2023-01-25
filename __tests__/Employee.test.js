fa0db24f42b673b8bd6d1e405559591443f4e4c1
hannahsvaughan <hannahvaughan421@gmail.com>
2023-01-25 16:02:20 -0500

    added questions for each object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,7 +5,7 @@ console.log(new Employee());
 test('create an employee object', () => {
     const employee = new Employee();
 
-    expect(employee.name).tobe('Test');
-    expect(employee.id).toEqual(-1);
-    expect(employee.email).tobe('test@gmail.com');
+    expect(employee.name).tobe('John');
+    expect(employee.id).toEqual(10);
+    expect(employee.email).tobe('john@gmail.com');
 });
