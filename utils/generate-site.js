a94df05d354f7e5f983ffe84acfb118ba77c95b7
hannahsvaughan <hannahvaughan421@gmail.com>
2023-01-25 17:37:04 -0500

    final changes for the day

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -3,17 +3,20 @@ const fs = require('fs');
 function writeFile(fileName, data) {
     return new Promise((resolve, reject) => {
         fs.writeFile(fileName, data, error => {
+            //if there is an error, reject the promise
             if (error) {
                 reject(error);
                 return;
             } resolve({
                 ok: true,
-                message: 'File created!'
+                message: 'File created! Inside the "dist" folder, your index.html file was created.'
             });
         });
     });
 };
 
+
+//copies CSS file to dist folder w/ new html file
 const copyFile = () => {
     return new Promise((resolve, reject) => {
         fs.copyFile('./src/style.css', './dist/style.css', err => {
@@ -22,7 +25,7 @@ const copyFile = () => {
                 return;
             } resolve({
                 ok: true,
-                message: 'File copied!'
+                message: 'File copied successfully!'
             });
         });
     });
