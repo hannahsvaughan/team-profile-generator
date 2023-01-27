8478b03b21504cce41b8cd39f1b49b5b0ade2caf
hannahsvaughan <hannahvaughan421@gmail.com>
2023-01-27 10:00:16 -0500

    removed team.js file

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const { default: inquirer } = require('inquirer');
-const Team = require('./lib/team');
-const Questions = require("./lib/Questions")
-// Function to initalize the app
-new Team().initializeTeam();
+// const Team = require('./lib/Team');
+const Questions = require("./lib/Questions");
+const Employee = require("./lib/Employee");
+// add others under here
 
-inquirer.prompt(new Questions("Manager").getLastQuestion())
+const Team = [];
+
+// Function to initalize the app
+
+// add array for team members to go in - promt the question and then the user selects "manager" then push the object to the team array then the question will loop again
+// push second object to team array again
+// use constructor to build the object
+// new Team().initializeTeam();
+
+// inquirer.prompt(new Questions("Manager").getLastQuestion())
+// .then {
+//     const 
+// }
+// answer.name
